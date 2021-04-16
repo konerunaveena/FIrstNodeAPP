b7cd94ca51e602c514ca12d7a8caed5a3d97b75f
Shamala Naveena <SN00732096@techmahindra.com>
2021-04-16 23:52:49 +0530

    Basic Routing

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  //console.log(req.url, req.headers, req.method);
+  const url = req.url;
+  if(url === "/") {
+    res.setHeader("Content-Type","text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write("<body><form action='/message' method='POST'><input type='text' /><button type='submit' >Send</button></form></body>");
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type","text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello From My Node JS Server !</h1></body>");
   res.write("</html>");
   res.end();
-
-  //process.exit();
 });
 
 server.listen(3001);
