abe26036b8276e32bd439acd63e51138cf5d80b9
Shamala Naveena <SN00732096@techmahindra.com>
2021-04-16 23:42:32 +0530

    sending response to browser

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req)
+  //console.log(req.url, req.headers, req.method);
+  res.setHeader("Content-Type","text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello From My Node JS Server !</h1></body>");
+  res.write("</html>");
+  res.end();
+
+  //process.exit();
 });
 
 server.listen(3001);
