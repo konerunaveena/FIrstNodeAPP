707ab22f51744d112743acff4338f2dc0b601d7d
Shamala Naveena <SN00732096@techmahindra.com>
2021-04-17 22:11:06 +0530

    Event Driven Execution

--- app.js
+++ app.js
@@ -5,31 +5,33 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
 
-  if(url === "/") {
-    res.setHeader("Content-Type","text/html");
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write("<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit' >Send</button></form></body>");
     res.write("</html>");
     return res.end();
   }
-  if(url === "/message" && method === "POST") {
+  if (url === "/message" && method === "POST") {
     console.log("coming");
     const body = [];
-    req.on('data',(chunk) => {
+    req.on('data', (chunk) => {
       console.log(chunk);
       body.push(chunk);
     });
     req.on('end', () => {
       const parseBody = Buffer.concat(body).toString();
       const message = parseBody.split("=")[1];
-      fs.writeFileSync('message.txt', message)
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+
   }
-  res.setHeader("Content-Type","text/html");
+  res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello From My Node JS Server !</h1></body>");
