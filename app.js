748cfaaf3217ac2e7536d4adc550c20f02071961
Shamala Naveena <SN00732096@techmahindra.com>
2021-04-19 12:55:35 +0530

    installed bodyparser , redirecting the pages thriugh middleware

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const http = require('http')
 const express = require('express')
-
+const bodyParser =  require('body-parser')
 const app = express();
 
-app.use((req,res,next) => {
+//app.use(bodyParser.urlencoded({extended :false}))  
+app.use(express.urlencoded({extended: true})) // if we are using express version above 4.16 use this else use above one 
+
+app.use('/add-product',(req,res,next) => {
+  res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>");
+});
+
+app.post('/product',(req,res,next)=> {
+  console.log(req.body)
+  res.redirect("/");
+})
+
+app.use('/',(req,res,next) => {
   res.send("<h1>Hello from expressjs");
 });
 
-const server = http.createServer(app);
-
-server.listen(3001);
+app.listen(3001);
