2c0eacddf66728529be14fcdb2b68add3aeb2640
Shamala Naveena <SN00732096@techmahindra.com>
2021-04-19 11:54:44 +0530

    installed the expressjs and removed the route.js file

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const http = require('http')
-const routes = require('./routes.js')
-const server = http.createServer(routes.handler);
+const express = require('express')
+
+const app = express();
+
+app.use((req,res,next) => {
+  res.send("<h1>Hello from expressjs");
+});
+
+const server = http.createServer(app);
 
 server.listen(3001);
