34bee2e996473388c64b5580d0acfe1fb5846faa
Shamala Naveena <SN00732096@techmahindra.com>
2021-05-22 13:02:53 +0530

    servering Html Pages and Returning 404 page

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const path = require('path')
 const express = require('express')
-const bodyParser =  require('body-parser')
-
+const bodyParser = require('body-parser')
+var cors = require('cors')
 const adminRoutes = require('./routes/admin')
-const shopRoutes =  require('./routes/shop')
+const shopRoutes = require('./routes/shop')
+const fs = require('fs');
+
 const app = express();
+app.use(cors());
+//app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false })) // if we are using express version above 4.16 use this else use above one 
+app.use(express.json());
 
-//app.use(bodyParser.urlencoded({extended :false}))  
-app.use(express.urlencoded({extended: true})) // if we are using express version above 4.16 use this else use above one 
-
-app.use(adminRoutes);
+app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 
-app.listen(3001);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname,"views","404.html"));
+  //res.status(404).sendFile(path.join(__dirname,"./","views","404.html"));
+})
+
+app.listen(3002);
