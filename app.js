ca9e135cbfb70a7dd1f4f79430885699145947ba
Shamala Naveena <SN00732096@techmahindra.com>
2021-05-24 19:12:45 +0530

    MVC structure

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-const fs = require('fs');
+const errorController = require('./controllers/error')
 
 const app = express();
 app.use(cors());
@@ -17,11 +17,9 @@ app.use(express.urlencoded({ extended: false })) // if we are using express vers
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use("/admin",adminData.routes);
+app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404',{pageTitle : 'Page Not Found',path : '/404', });
-})
+app.use(errorController.get404)
 
 app.listen(3002);
